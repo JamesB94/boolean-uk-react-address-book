032d269c2c3d5a1769b0da11acc340c731ae8c84
James Byrne <james.c.byrne1994@gmail.com>
2022-10-04 12:18:51 +0100

    first push to github

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Link, Route, Routes } from "react-router-dom"
 import ContactsList from "./components/ContactsList"
 import ContactsAdd from "./components/ContactsAdd"
 import ContactsView from "./components/ContactsView"
+import contacts from "../db/db.json"
 import "./styles/styles.css"
 
 export default function App() {
   const [contacts, setContacts] = useState([])
+
+  console.log(contacts)
   
   //TODO: Load all contacts on useEffect when component first renders
+
+  useEffect((contacts) => {
+    return fetch('http://localhost:3333/list')
+    .then(data => data.json())
+  },[] );
+
 
   return (
     <>
@@ -22,6 +31,9 @@ export default function App() {
       </nav>
       <main>
         <Routes>
+          <Route path="ContactsList"/>
+          <Route path="ContactsAdd"/>
+          <Route/>
           {/* TODO: Add routes here  */}
         </Routes>
       </main>
