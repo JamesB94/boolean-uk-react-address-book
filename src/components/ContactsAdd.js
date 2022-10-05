c7d0c279a8c3a86176596a6bc460dc00c3682b43
James Byrne <james.c.byrne1994@gmail.com>
2022-10-05 12:09:02 +0100

    added POST to contact Form

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -4,8 +4,9 @@ import { useNavigate } from "react-router-dom";
 function ContactsAdd(props) {
 
   const [firstName, setFirstName] = useState([])
-
-  console.log(firstName)
+  const [lastName, setLastName] = useState([])
+  const [street, setStreetName] = useState([])
+  const [city, setCityName] = useState([])
 
   // setContacts and contacts must be passed as props
   // to this component so new contacts can be added to the
@@ -16,8 +17,19 @@ function ContactsAdd(props) {
   //TODO: Implement controlled form
   //send POST to json server on form submit
 
-  function handleSubmit(e) {
+  const handleSubmit =(e) => {
     e.preventDefault()
+    const contacts ={firstName,lastName,street,city }
+
+    fetch('http://localhost:4000/contacts', {
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify(contacts)
+    })
+
+
+    //  console.log(contacts)
+
 
   }
 
@@ -26,23 +38,29 @@ function ContactsAdd(props) {
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" value={firstName} onChange={(e) => setFirstName()} required />
+      <input id="firstName" name="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input id="lastName" name="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input id="street" name="street" type="text" value={street} onChange={(e) => setStreetName(e.target.value)}  required/>
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input id="city" name="city" type="text" value={city} onChange={(e) => setCityName(e.target.value)} required/>
 
       <div className="actions-section">
         <button className="button blue" type="submit">
           Create
         </button>
       </div>
+      <p>{firstName}</p>
+      <p>{lastName}</p>
+      <p>{street}</p>
+      <p>{city}</p>
     </form>
+
+    
   )
 }
 
